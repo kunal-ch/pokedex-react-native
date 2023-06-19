35c37d59458cc7223c3bd6242eaea064d0e48b10
kunal <kunalch36@gmail.com>
2023-06-19 18:15:45 +0530

    Navigation Setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native"
 import PokemonListScreen from "./src/screens/PokemonListScreen";
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
-  return <SafeAreaView>
-    <PokemonListScreen/>
-    {/* <PokemonDetailScreen /> */}
-  </SafeAreaView>
+  return (
+    <NavigationContainer>
+      <PokemonListScreen/>
+    </NavigationContainer>
+  );
 }
 
 export default App;
