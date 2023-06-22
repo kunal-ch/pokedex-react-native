745b754ce01a2dab23c81141ad525e60fbdb68cc
kunal <kunalch36@gmail.com>
2023-06-22 18:27:23 +0530

    react navigation issue fix

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import { SafeAreaView } from "react-native"
 import PokemonListScreen from "./src/screens/PokemonListScreen";
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PokemonDetailScreen from "./src/screens/PokemonDetailScreen";
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <PokemonListScreen/>
-    </NavigationContainer>
+  return (<NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={PokemonListScreen} />
+      <Stack.Screen name="Detail" component={PokemonDetailScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
